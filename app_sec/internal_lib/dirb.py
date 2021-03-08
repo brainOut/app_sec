6a5df16c8415a52985fc090eb47df07830150215
Maxime BONGIORNO <maxime@kiboko.fr>
2021-03-08 17:02:32 +0100

    add wordlist and dirb refacto

--- app_sec/internal_lib/dirb.py
+++ app_sec/internal_lib/dirb.py
@@ -4,22 +4,29 @@ import sys
 
 
 class fuzz_url:
-    def __init__(self, url, wordlist):
+
+    def __init__(self, url, quiet=False):
         self.url = url
-        self.wordlist = wordlist
+        self.quiet = quiet
+        self.wordlist = open("wordlist.txt", 'r').readlines()
 
     def dirchecker(self):
-
+        url_ok = []
         for paths in self.wordlist:
+            paths = paths.strip("\r\n")
             try:
                 # print('http://' + self.url + '/' + paths)
                 response_code = requests.get('http://' + self.url + '/' + paths).status_code
+                if not self.quiet:
+                    print(f"trying http://{self.url}/{paths}")
+
                 if response_code != 404:
-                    print(f"http://{self.url}/{paths} ({response_code})")
+                    url_ok = f"http://{self.url}/{paths} ({response_code})"
             except Exception:
                 print(f"exception {Exception}")
+        print(url_ok)
 
 
 if __name__ == "__main__":
-    fuzzer = fuzz_url('127.0.0.1:8000', ["toto", "tata", "admin"])
+    fuzzer = fuzz_url('127.0.0.1:8000', True)
     fuzzer.dirchecker()
